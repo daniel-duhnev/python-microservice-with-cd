c24b134de296904bb7d92bf3fc0933a37479a6cc
Daniel Duhnev <daniel.duhnev@sky.uk>
2023-02-15 15:27:33 +0000

    improve main.py test coverage

--- test_main.py
+++ test_main.py
@@ -8,3 +8,11 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Wikipedia API. Call /search or /wiki"}
+
+def test_search_wiki():
+    response = client.get("/search/War_Goddess")
+    assert response.status_code == 200
+
+def test_wiki_logic():
+    response = client.get("/wiki/War_Goddess")
+    assert response.status_code == 200
